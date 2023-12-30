3128e700b533a0a3c4e1fd3dd35119234436d843
ThomasCT1999 <123325434+ThomasCT1999@users.noreply.github.com>
2023-12-30 20:09:27 +0000

    update

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const User = require('./models/user'); // Assuming you have a User model
+const Credentials = require('./models/credentials');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const url = 'mongodb://luckyowlzz:ACrp4yzP7RNscpgq@cluster0.7mgu4ox.mongodb.net/taskmanagement';
 
 // Connect to MongoDB
-mongoose.connect('mongodb://luckyowlzz:ACrp4yzP7RNscpgq@cluster0.7mgu4ox.mongodb.net/taskmanagement', {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // Middleware
+app.use(cors()); // Add this line for CORS
 app.use(bodyParser.json());
 
 // Registration route
@@ -22,15 +25,31 @@ app.post('/register', async (req, res) => {
 
     // Validate the data if needed
 
-    // Create a new user
-    const newUser = new User({ username, password, email });
+    // Create a new credentials instance
+    const newCredentials = new Credentials({ username, password, email });
 
-    // Save the user to the database
-    await newUser.save();
+    // Save the credentials to the "credentials" collection
+    await newCredentials.save();
 
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error('Registration error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Route to list all collections
+app.get('/collections', async (req, res) => {
+  try {
+    const collectionsList = await mongoose.connection.db.listCollections().toArray();
+    const collectionNames = collectionsList.map(collection => collection.name);
+    res.status(200).json({ collections: collectionNames });
+  } catch (error) {
+    console.error('Error listing collections:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
